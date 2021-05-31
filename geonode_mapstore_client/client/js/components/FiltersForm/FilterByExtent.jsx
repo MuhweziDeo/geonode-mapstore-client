1df0bb33b893641ad5765451b61d17b658506502
Muhwezi Aggrey <aggrey256@gmail.com>
2021-05-31 11:23:37 +0300

    update useEffect hook

--- geonode_mapstore_client/client/js/components/FiltersForm/FilterByExtent.jsx
+++ geonode_mapstore_client/client/js/components/FiltersForm/FilterByExtent.jsx
@@ -29,14 +29,26 @@ function ZoomTo({
     useEffect(() => {
         if (map && extent && !once.current) {
             const [
-                minx, miny, maxx, maxy
+                aMinx, aMiny, aMaxx, aMaxy,
+                bMinx, bMiny, bMaxx, bMaxy
             ] = extent.split(',');
             const projection = map.getView().getProjection().getCode();
-            const bounds = reprojectBbox([minx, miny, maxx, maxy], 'EPSG:4326', projection);
+            let bounds;
+            const aBounds = reprojectBbox([aMinx, aMiny, aMaxx, aMaxy], 'EPSG:4326', projection);
+            if (bMinx !== undefined && bMiny !== undefined && bMaxx !== undefined && bMaxy !== undefined) {
+                const bBounds = reprojectBbox([bMinx, bMiny, bMaxx, bMaxy], 'EPSG:4326', projection);
+                // if there is the second bbox we should shift the minimum x value to correctly center the view
+                // the x of the [A] bounds needs to be shifted by the width of the [B] bounds
+                const minx = aBounds[0] - (bBounds[2] - bBounds[0]);
+                bounds = [minx, aBounds[1], aBounds[2], aBounds[3]];
+            } else {
+                bounds = aBounds;
+            }
             map.getView().fit(bounds, {
                 size: map.getSize(),
                 duration: 300
             });
+            // ensure to avoid other fit action by setting once to true
             once.current = true;
         }
     }, [ extent ]);
